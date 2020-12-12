87db3c3b127fb1d32d0d86c744577c51a7d0e821
aniledev <61761502+aniledev@users.noreply.github.com>
2020-12-12 11:47:18 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./index.js",
   output: {
     path: __dirname + "/dist",
     filename: "index_bundle.js",
