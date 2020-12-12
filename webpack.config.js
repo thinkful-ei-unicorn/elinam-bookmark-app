3a6b9e9b3cf703f792908934039b1c57b3fa83d4
aniledev <elinamcgill@gmail.com>
2020-12-12 10:05:22 -0500

    Updates

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "index_bundle.js",
+    publicPath: "/bookmark-app-redo/",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "/src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
