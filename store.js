365dbeb7cdaa9a6c2b39a70dea4afbfd99f35e4e
aniledev <61761502+aniledev@users.noreply.github.com>
2020-12-12 23:28:26 -0500

    Update store.js

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
-import * as api from "./api";
-import * as index from "./index";
-import * as store from "./store";
+import * as api from "./api.js";
+import * as index from "./index.js";
+import * as store from "./store.js";
 
 // THIS MODULE ONLY CONTAINS STORE VARIABLE AND FUNCTIONS THAT CHANGE THE STATE OF THE STORE
 let STORE = {
