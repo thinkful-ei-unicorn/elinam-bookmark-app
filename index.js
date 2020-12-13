1fe88c8156839426ff0e71ff5701ab88d2b125a4
aniledev <61761502+aniledev@users.noreply.github.com>
2020-12-12 22:53:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import $ from "./jquery";
-import api from "./api";
+import api from "/dist/api.js";
 import events from "./events";
 import store from "./store";
 import templates from "./templates";
